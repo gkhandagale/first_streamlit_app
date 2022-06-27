a65c36bde25513acc0d33f45a161f1958938b205
gkhandagale <68633648+gkhandagale@users.noreply.github.com>
2022-06-27 14:15:03 +0530

    Update streamlit_app.py
    added one more menu option and emojee for all

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Omlette')
-streamlit.text('Kale, Spinach and Rocket Smothie')
-streamlit.text('Hard Boiled Free Range Egg')
+streamlit.text('🥣 Omega 3 and Blueberry Omlette')
+streamlit.text('🥗 Kale, Spinach and Rocket Smothie')
+streamlit.text('🐔 Hard Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Test')
